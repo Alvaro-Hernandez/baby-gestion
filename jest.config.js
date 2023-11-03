20a1a2637a7e04f01d0f41cca511a43e13e18c9c
Alvaro-Hernandez <alvaropineda606@gmail.com>
2023-11-03 00:23:52 -0600

    :heavy_plus_sign:Add dependency and configurations for tests

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   preset: 'react-native',
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|react-native-button|@react-native-community|@react-native)/)',
+  ],
+  testPathIgnorePatterns: ['/node_modules/', '/path/to/ignore/'],
+  testMatch: ['**/__tests__/**/*.test.js'],
 };
