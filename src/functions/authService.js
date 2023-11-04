7da0085692c84100ed358ce6be1f751f22977cee
Alvaro-Hernandez <alvaropineda606@gmail.com>
2023-11-03 19:12:59 -0600

    :zap:Add function for patient authentication

---
+++ src/functions/authService.js
@@ -0,0 +1,18 @@
+import firestore from '@react-native-firebase/firestore';
+import {Alert} from 'react-native';
+
+export const verificarCredencial = async (credencial, navigation) => {
+  try {
+    const docRef = firestore().collection('cartilla').doc(credencial);
+    const doc = await docRef.get();
+
+    if (doc.exists) {
+      navigation.navigate('MainApp');
+      Alert.alert('Éxito', 'Acceso permitido');
+    } else {
+      Alert.alert('Error', 'Credencial perinatal no encontrada');
+    }
+  } catch (error) {
+    Alert.alert('Error', 'Hubo un error al verificar la credencial');
+  }
+};
